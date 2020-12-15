2dc55f3eb670d932d826a83ef0f8eef6f0386ff3
haobing <liamwang@aliyun.com>
2020-12-15 16:03:36 +0800

    添加 生成器

---
+++ generator.js
@@ -0,0 +1,25 @@
+require('./polyfill/array.js')
+
+/**
+ * 
+ * @param {number} start 开始值
+ * @param {number} stop 结束值
+ * @param {number} step 间隔
+ */
+function range (start, stop, step) {
+    return Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step))
+}
+
+/**
+ * 获取未来年份列表
+ * @param {Number} count 年份数量
+ */
+function getFutureYears (count) {
+    const currentYear = (new Date()).getFullYear()
+    return range(currentYear, count, 1)
+}
+
+module.exports = {
+    range,
+    getFutureYears
+}
